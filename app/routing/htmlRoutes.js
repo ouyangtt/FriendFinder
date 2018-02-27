02e8effd6bac1354c93b758a6d5c6ec58f463b11
TUNG TUNG OUYANG <ouyangtt@TUNGs-MacBook-Air.local>
2018-02-27 15:32:01 -0800

    initiate

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,2 @@
+var friendData = require('../data/friends.js');
+var path = require('path');
---
+++ server.js
@@ -0,0 +1,6 @@
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var friends = require('./app/data/friends.js');
+

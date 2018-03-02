86c2502301da1de50ee09554c4022abbf2972ee9
TUNG TUNG OUYANG <ouyangtt@TUNGs-MacBook-Air.local>
2018-03-02 02:02:58 -0800

    adding api routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,2 +1,42 @@
 var friends = require("../data/friends.js");
 
+
+module.exports = function apiRoutes(app) {
+    app.get("/api/friends", function(req, res) {
+        res.json(friends);
+    });
+
+ 
+    app.post("/api/friends", function(req, res) {
+       
+        var matchFriendInfo = bestMatch(friends, req.body);
+        friends.push(req.body);
+        res.json(matchFriendInfo);
+
+    });
+};
+
+function bestMatch(friends, newFriend) {
+    var closestDifference;
+    var matchFriend;
+    for (var i = 0; i < friends.length; i++) {
+        var friend = friends[i];
+        var difference = 0;
+        for (var j = 0; j < newFriend.scores.length; j++) {
+            var answerA = newFriend.scores[j];
+            var answerB = friend.scores[j];
+            difference = difference + (Math.abs(answerA - answerB));
+        }
+        if (difference < closestDifference || closestDifference === undefined) {
+            closestDifference = difference;
+            matchFriend = friend;
+        }
+        return {
+            name: matchFriend.name,
+        photo: matchFriend.photo
+
+        };
+    }
+}
+
+

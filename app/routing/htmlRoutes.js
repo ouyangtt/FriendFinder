cdba16b2a136c41b58311e72807a4dd798780b0e
TUNG TUNG OUYANG <ouyangtt@TUNGs-MacBook-Air.local>
2018-03-02 02:03:43 -0800

    adding html routes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,2 +1,16 @@
 var path = require('path');
 
+
+
+module.exports = function htmlRoutes(app) {
+
+    app.get("/survey", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
+  
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+
+};
+
